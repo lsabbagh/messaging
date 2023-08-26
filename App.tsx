c90efb2f9d833edee3a9d6f1b9d3415fb5443d4e
kareen <kareenmilli3@gmail.com>
2023-08-26 19:40:33 +0300

    creating first app

--- App.tsx
+++ App.tsx
@@ -1,24 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { ImageBackground, StatusBar, StyleSheet, View } from "react-native";
+const slides = [{
+ id: 1,
+ ImageBackground:require('./assets/images/Hey.png')
+  },
+  {
+    id: 2,
+ ImageBackground:require('./assets/images/2.png')
+  },
+{
+  id: 3,
+ ImageBackground:require('./assets/images/3.png')
+  }
+]
+
 export default function App(){
+  const [showHomePage, setShowHomePage] = useState(false);
+  if(!showHomePage){
+    return(
+      <AppIntoSlider/></AppIntoSlider>
+    )
+  }
   return(
-    <View style={styles.container}>
-      <StatusBar hidden />
-      <ImageBackground
-      source={require('./assets/images/Hey.png')}
-      resizeMode='cover'
-      style={{flex: 1}}>
-      </ImageBackground>
-    </View>
-  );
-  }
+    <View style={{
+     flex: 1,
+     justifyContent: 'center',
+     
+    }}
+  
+    />)}
 
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1 ,
-    backgroundColor:'#fff',},
-  }
-);
 
